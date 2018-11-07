be501bef424a5a7a6e3574b9ba62c825dc89a583
Super169 <Super169@126.com>
2018-11-07 18:40:56 +0800

    New feature for UI & XML

---
+++ MyUtil/UI.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyUtil
+{
+    public static partial class UI
+    {
+        public static void UpdateTextbox(System.Windows.Controls.TextBox tb, string xml, bool async = false)
+        {
+            if (System.Windows.Threading.Dispatcher.FromThread(System.Threading.Thread.CurrentThread) == null)
+            {
+                if (async)
+                {
+                    System.Windows.Application.Current.Dispatcher.BeginInvoke(
+                      System.Windows.Threading.DispatcherPriority.Normal,
+                      (Action)(() => UpdateTextbox(tb, xml)));
+                }
+                else
+                {
+                    System.Windows.Application.Current.Dispatcher.Invoke(
+                      System.Windows.Threading.DispatcherPriority.Normal,
+                      (Action)(() => UpdateTextbox(tb, xml)));
+                }
+                return;
+            }
+            tb.Text = xml;
+        }
+    }
+}
